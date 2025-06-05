f41f0c7004fbc99ea6f6aeb30e6612faee48ea6a
fr0st-even <iosef.joelcv@gmail.com>
2025-06-04 22:31:06 -0600

    Car

--- javascript/auth/logout.js
+++ javascript/auth/logout.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     
     document.getElementById("btn-logout").addEventListener("click", () => {
         localStorage.removeItem("user_id");
-        window.location.href = '/frontend/login.html';
+        window.location.href = '/login.html';
     });
 });
